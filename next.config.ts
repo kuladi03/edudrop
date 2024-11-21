2156d7d3e75fd807a7784cc4329218ef4e70eec9
kuladi03 <kuladi03@gmail.com>
2024-11-21 18:28:48 +0530

    Updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true, // Ensures URLs end with a slash
-  images: {
-    unoptimized: true, // Necessary for static export
-  },
-  basePath: '/edudrop', // Replace with your GitHub repo name
-  assetPrefix: '/edudrop', // Replace with your GitHub repo name
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
